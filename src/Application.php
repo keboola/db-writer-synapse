91d00eaf0589798889725b22226f6d7c885c9145
Ondra Jodas <ondrej.jodas@keboola.com>
2021-03-05 09:36:13 +0100

    check if manifest exists

--- src/Application.php
+++ src/Application.php
@@ -135,8 +135,15 @@ class Application extends BaseApplication
 
     private function getManifest(string $tableId): array
     {
+        $tablePath = $this['parameters']['data_dir'] . '/in/tables/' . $tableId . '.csv.manifest';
+        if (!file_exists($tablePath)) {
+            throw new UserException(sprintf(
+                'Table "%s" in storage input mapping cannot be found.',
+                $tableId
+            ));
+        }
         return json_decode(
-            (string) file_get_contents($this['parameters']['data_dir'] . '/in/tables/' . $tableId . '.csv.manifest'),
+            (string) file_get_contents($tablePath),
             true
         );
     }
